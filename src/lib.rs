ebe870bb9ab237da92352ee5e3d63b6fa2db6597
Android789515 <derflug@mailfence.com>
2025-06-07 02:03:45 -0500

    feat: random item function
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! # Random Item
+//!
+//! `random_item` lets you get a random item from a sequential collection.
+
+/// Gets a random item from a sequential collection.
+/// # Example
+/// ```
+/// use random_item::random_item;
+/// 
+/// let numbers = (0..99).collect::<Vec<u8>>();
+///
+/// assert!(numbers.contains(random_item(&numbers)));
+/// ```
+pub fn random_item<Type>(collection: &[Type]) -> &Type {
+    &collection[rand::random_range(0..collection.len())]
 }
 
 #[cfg(test)]
@@ -7,8 +20,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn gets_item_from_vec() {
+        let numbers = (0..99).collect::<Vec<u8>>();
+
+        assert!(numbers.contains(random_item(&numbers)));
+    }
+
+    #[test]
+    fn gets_item_from_array() {
+        let letters = ['a', 'b', 'c'];
+
+        assert!(letters.contains(random_item(&letters)));
     }
 }
